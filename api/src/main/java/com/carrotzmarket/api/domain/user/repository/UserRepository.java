5e4b3f9f8b28a4373d974df8552670ab334f9af5
makernaru-test00 <qweqwerty12321@gmail.com>
2024-12-24 22:51:11 +0900

    chore: 사용자 로그인 제한 기능에 따른 리포지토리 변경

--- api/src/main/java/com/carrotzmarket/api/domain/user/repository/UserRepository.java
+++ api/src/main/java/com/carrotzmarket/api/domain/user/repository/UserRepository.java
@@ -3,22 +3,31 @@ package com.carrotzmarket.api.domain.user.repository;
 import com.carrotzmarket.db.region.RegionEntity;
 import com.carrotzmarket.db.user.UserEntity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepository{
 
+    @PersistenceContext
     private final EntityManager em;
 
+    @Transactional
     public void save(UserEntity user) {
-        em.persist(user);
+        UserEntity managedUser = em.merge(user);
+        em.flush();
+        System.out.println("Entity contained in persistence context: " + em.contains(managedUser)); // 변경된 객체 확인
     }
 
+    public Optional<RegionEntity> findRegionById(Long regionid){
+        RegionEntity region = em.find(RegionEntity.class, regionid);
+        return Optional.ofNullable(region);
+    }
 
     public Optional<UserEntity> findByLoginId(String loginId) {
         return em.createQuery("SELECT u FROM UserEntity u WHERE u.loginId = :loginId", UserEntity.class)
@@ -27,13 +36,11 @@ public class UserRepository{
                 .findFirst();
     }
 
-
     public void deleteUserRegionsByUserId(Long userId) {
         em.createQuery("DELETE FROM UserRegionEntity ur WHERE ur.user.id = :userId")
                 .setParameter("userId", userId)
                 .executeUpdate();
     }
-
 
     public void deleteByLoginId(String loginId) {
         UserEntity user = em.createQuery("SELECT u FROM UserEntity u WHERE u.loginId = :loginId", UserEntity.class)
@@ -42,12 +49,6 @@ public class UserRepository{
 
         deleteUserRegionsByUserId(user.getId());
         em.remove(user);
-    }
-
-
-    public Optional<RegionEntity> findRegionById(Long regionid){
-        RegionEntity region = em.find(RegionEntity.class, regionid);
-        return Optional.ofNullable(region);
     }
 
     public Optional<UserEntity> findById(Long id) {
