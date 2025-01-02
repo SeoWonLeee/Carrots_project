5b50f2d3ef2b0a1da0c11b84095741a7f891ba7b
choiminu <alsdn4590@gmail.com>
2025-01-02 15:47:10 +0900

    feat : 메인 배너 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/Header";
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/Layout";
 import ContentCategory from "./components/content/ContentCategory";
+import MainContent from "./components/content/MainContent";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
         <Route path="/" element={
               <div>
+                <MainContent/>
                 <ContentCategory />
               </div>
             } />
