d0a79d83f30e9f6922b27e600172e0c863adf7fe
dltjdnjs <scw.dltjdnjs@gmail.com>
2025-01-07 14:27:44 +0900

    feat: 상품 거래 관련 DTO 추가

--- api/src/main/java/com/carrotzmarket/api/domain/transaction/dto/TransactionStatusUpdateRequest.java
+++ api/src/main/java/com/carrotzmarket/api/domain/transaction/dto/TransactionStatusUpdateRequest.java
@@ -3,12 +3,18 @@ package com.carrotzmarket.api.domain.transaction.dto;
 import com.carrotzmarket.db.transaction.TransactionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionStatusUpdateRequest {
     private Long productId;
     private Long authorId;
+    private LocalDate transactionDate;
+    private LocalDateTime tradingHours;
     private TransactionStatus status;
-
 }
